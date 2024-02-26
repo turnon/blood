ef42908cfe0150ecdaa6b6afcec190c51987529a
ken <block24block@gmail.com>
2024-02-26 18:40:16 +0800

    avoid different module order

--- lib/blood.rb
+++ lib/blood.rb
@@ -9,13 +9,36 @@ module Blood
   def self.source(mods)
     hier = Hash.new{ |h, k| h[k] = Set.new }
     mods.each do |mod|
-      ances = mod.ancestors
+      ances = mod.ancestors.reduce([]) do |arr, a|
+        next arr << a if Class === a
+        arr << Modules.new unless Modules === arr[-1]
+        arr[-1].add(a)
+        arr
+      end
       ances.each_with_index do |child, i|
         parent = ances[i + 1]
         hier[parent] << child
       end
     end
     Node.new(BasicObject, hier)
+  end
+
+  class Modules
+    def add(mod)
+      (@mods ||= []) << mod
+    end
+
+    def to_s
+      @to_s ||= (@mods.count == 1 ? @mods[0].to_s : @mods.to_s)
+    end
+
+    def hash
+      to_s.hash
+    end
+
+    def eql?(other)
+      to_s.eql?(other.to_s)
+    end
   end
 
   class Node
